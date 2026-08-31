78cf1a0db006b068b57d8a83998003baf0b52f2c
saisaynoomleng <saileng9723@gmail.com>
2026-08-31 16:16:25 -0400

    sanity_setup

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,18 @@
-'use client'
+'use client';
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { apiVersion, dataset, projectId } from './src/sanity/env';
+import { schema } from './src/sanity/schemaTypes';
+import { structure } from './src/sanity/structure';
+import { media } from 'sanity-plugin-media';
 
 export default defineConfig({
   basePath: '/studio',
@@ -20,9 +21,8 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    structureTool({ structure }),
+    media(),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
-})
+});
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,7 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/structure';
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title('snoomleng')
+    .items([S.divider().title('Operations'), S.divider().title('Blogs')]);
