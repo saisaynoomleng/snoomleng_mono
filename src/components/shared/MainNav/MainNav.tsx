1b65dc3fefd6c6663a9fde20e85f21a2b525742a
saisaynoomleng <saileng9723@gmail.com>
2026-08-31 22:31:20 -0400

    components_header

--- src/components/shared/MainNav/MainNav.tsx
+++ src/components/shared/MainNav/MainNav.tsx
@@ -1,5 +1,55 @@
+import { urlFor } from '@/sanity/lib/image';
+import { sanityFetch } from '@/sanity/lib/live';
+import { MAIN_NAV_QUERY } from '@/sanity/lib/query';
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
+import { DesktopNav, MobileNav } from './Nav';
 
-export const MainNav = (): React.JSX.Element => {
-  return <header></header>;
+type MainNavProps = {
+  className?: string;
+};
+
+export const MainNav = async ({
+  className,
+}: MainNavProps): Promise<React.JSX.Element> => {
+  const { data: menu } = await sanityFetch({ query: MAIN_NAV_QUERY });
+
+  if (!menu) notFound();
+
+  const { imageUrl, imageAlt } = menu;
+
+  return (
+    <header
+      className={twMerge(
+        clsx(
+          'flex items-center justify-between px-4 py-2 max-w-7xl mx-auto w-full shadow',
+          className,
+        ),
+      )}
+    >
+      <div className="w-25">
+        {imageUrl && imageAlt && (
+          <Link href="/" className="overflow-hidden aspect-square max-w-25">
+            <Image
+              src={urlFor(imageUrl).width(100).height(100).format('webp').url()}
+              alt={imageAlt}
+              priority
+              width={50}
+              height={50}
+              sizes="(max-width: 50px) 100vw, 33vw"
+              className="max-w-full object-cover"
+            />
+          </Link>
+        )}
+      </div>
+
+      <DesktopNav menu={menu.navigations} />
+
+      <MobileNav menu={menu.navigations} />
+    </header>
+  );
 };
---
+++ src/components/shared/MainNav/Nav.tsx
@@ -0,0 +1,146 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { MAIN_NAV_QUERY_RESULT } from '@/sanity/types';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { notFound, usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { twMerge } from 'tailwind-merge';
+
+type NavProps = {
+  className?: string;
+  menu: NonNullable<MAIN_NAV_QUERY_RESULT>['navigations'];
+};
+
+export const DesktopNav = ({ className, menu }: NavProps) => {
+  const pathname = usePathname();
+
+  if (!menu) notFound();
+
+  return (
+    <nav
+      role="navigation"
+      aria-label="main menu"
+      className={twMerge(
+        clsx(
+          'font-heading flex gap-x-4 md:gap-x-6 items-center max-md:hidden',
+          className,
+        ),
+      )}
+    >
+      {menu.map((m) => (
+        <React.Fragment key={m._key}>
+          {m.isButton ? (
+            <Button type="button" asChild>
+              <Link href={m.href as string} className="font-bold">
+                {m.label}
+              </Link>
+            </Button>
+          ) : (
+            <Link
+              href={m.href as string}
+              className={twMerge(
+                clsx(
+                  'hover:underline underline-offset-4 decoration-wavy decoration-primary font-semibold',
+                  pathname === m.href && 'underline',
+                ),
+              )}
+              rel={
+                m.href?.startsWith('http') ? 'noreferrer noindex' : undefined
+              }
+              target={m.href?.startsWith('http') ? '_blank' : ''}
+            >
+              {m.label}
+            </Link>
+          )}
+        </React.Fragment>
+      ))}
+    </nav>
+  );
+};
+
+export const MobileNav = ({
+  className,
+  menu,
+}: NavProps): React.JSX.Element | null => {
+  const [open, setOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    document.body.style.overflowY = open ? 'hidden' : '';
+
+    return () => {
+      document.body.style.overflowY = '';
+    };
+  }, [open]);
+
+  if (!menu) return null;
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className="relative z-50 shadow-none! md:hidden"
+        onClick={() => setOpen((prevOpen) => !prevOpen)}
+      >
+        {open ? (
+          <span>
+            <IoClose />
+          </span>
+        ) : (
+          <span>
+            <RxHamburgerMenu />
+          </span>
+        )}
+      </Button>
+
+      <nav
+        role="navigation"
+        aria-label="main menu"
+        className={twMerge(
+          clsx(
+            'md:hidden flex flex-col fixed bg-primary/10 inset-0 backdrop-blur-2xl transition-transform duration-200 ease-in-out justify-center items-center gap-y-4 z-40',
+            open ? 'translate-y-0' : '-translate-y-full',
+            className,
+          ),
+        )}
+      >
+        {menu.map((n) => (
+          <React.Fragment key={n._key}>
+            {n.isButton ? (
+              <Button type="button" asChild>
+                <Link
+                  href={n.href as string}
+                  className="font-bold!"
+                  onClick={() => setOpen(false)}
+                >
+                  {n.label}
+                </Link>
+              </Button>
+            ) : (
+              <Link
+                href={n.href as string}
+                className={twMerge(
+                  clsx(
+                    'hover:underline underline-offset-4 decoration-wavy decoration-primary font-semibold',
+                    pathname === n.href && 'underline',
+                  ),
+                )}
+                rel={
+                  n.href?.startsWith('http') ? 'noreferrer noindex' : undefined
+                }
+                target={n.href?.startsWith('http') ? '_blank' : ''}
+                onClick={() => setOpen(false)}
+              >
+                {n.label}
+              </Link>
+            )}
+          </React.Fragment>
+        ))}
+      </nav>
+    </>
+  );
+};
