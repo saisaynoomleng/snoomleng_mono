38e6dcd30a0c8d7283f54090234b7fa2f650a43d
saisaynoomleng <saileng9723@gmail.com>
2026-09-02 14:23:10 -0400

    contact-page

--- src/app/(frontend)/contact-me/page.tsx
+++ src/app/(frontend)/contact-me/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { Bounded, ContactSection } from '@/components/shared';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contact Me',
+  description:
+    'Have a project, opportunity, or question? Get in touch with Snoomleng to discuss web development, collaboration, or freelance work.',
+};
 
 const ContactMePage = () => {
-  return <div>ContactMePage</div>;
+  return (
+    <Bounded>
+      <ContactSection />
+    </Bounded>
+  );
 };
 
 export default ContactMePage;
