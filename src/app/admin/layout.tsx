67a43273c3f3e3d53f4ae43c2273cad4d64f7e64
saisaynoomleng <saileng9723@gmail.com>
2026-09-03 14:59:54 -0400

    query-provider

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,16 +1,20 @@
+import { QueryProvider } from '@/components/admin/QueryProvider';
 import { AdminSidebar } from '@/components/admin/Sidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 export default function AdminLayout({ children }: LayoutProps<'/admin'>) {
   return (
     <>
-      <SidebarProvider>
-        <AdminSidebar />
-        <main>
-          <SidebarTrigger className="border-0" />
-          {children}
-        </main>
-      </SidebarProvider>
+      <QueryProvider>
+        <SidebarProvider>
+          <AdminSidebar />
+          <main>
+            <SidebarTrigger className="border-0" />
+
+            {children}
+          </main>
+        </SidebarProvider>
+      </QueryProvider>
       ;
     </>
   );
---
+++ src/lib/queryKeys.ts
@@ -0,0 +1,5 @@
+export const queryKeys = {
+  contacts: {
+    all: ['contacts'] as const,
+  },
+};
